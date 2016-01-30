2517bd71321495d87e50504d7b94785e2c280750
Kylian Kuijer <k.kuijer@students.uu.nl>
2016-01-30 16:25:36 +0100

    Cauldron rainbow

--- ScreenGame/Assets/BubblingIntensifiesScript.cs
+++ ScreenGame/Assets/BubblingIntensifiesScript.cs
@@ -12,20 +12,20 @@ public class BubblingIntensifiesScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Vector3 distortion = Vector3.zero;
-		switch(GameObject.Find("Master").GetComponent<Master>().Difficulty)
-		{
-		case 10: distortion.x = distortion.y = Random.Range(5, 10);
-		case 9: break;
-		case 8:	 distortion.x = distortion.y = Random.Range(3, 5);
-		case 7: break;
-		case 6:  distortion.x = distortion.y = Random.Range(1, 3);
-		case 5: break;
-		case 4: distortion.x = distortion.y = Random.Range(0, 1);
-		case 3: break;
-		case 2: distortion.x = distortion.y = Random.Range(0, 0);
-		case 1: break;
-		default: break;
-		}
+//		switch(GameObject.Find("Master").GetComponent<Master>().Difficulty)
+//		{
+//		case 10: distortion.x = distortion.y = Random.Range(5, 10);
+//		case 9: break;
+//		case 8:	 distortion.x = distortion.y = Random.Range(3, 5);
+//		case 7: break;
+//		case 6:  distortion.x = distortion.y = Random.Range(1, 3);
+//		case 5: break;
+//		case 4: distortion.x = distortion.y = Random.Range(0, 1);
+//		case 3: break;
+//		case 2: distortion.x = distortion.y = Random.Range(0, 0);
+//		case 1: break;
+//		default: break;
+//		}
 		transform = Initial + distortion;
 	}
 }
