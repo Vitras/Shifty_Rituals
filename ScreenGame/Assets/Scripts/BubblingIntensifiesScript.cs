1f51e81f4d03ab9c0d5e7395ab6a4850ff109593
Kylian Kuijer <k.kuijer@students.uu.nl>
2016-01-30 20:42:48 +0100

    Opschoning en BUBBLING INTENSIFIES

--- ScreenGame/Assets/Scripts/BubblingIntensifiesScript.cs
+++ ScreenGame/Assets/Scripts/BubblingIntensifiesScript.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class BubblingIntensifiesScript : MonoBehaviour {
 
 	public Vector3 Initial;
+	public int frame;
 	// Use this for initialization
 	void Start () {
 		Initial = transform.position;
@@ -11,21 +12,26 @@ public class BubblingIntensifiesScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		frame++;
+		if (frame >= 4)
+		{
 		Vector3 distortion = Vector3.zero;
-//		switch(GameObject.Find("Master").GetComponent<Master>().Difficulty)
-//		{
-//		case 10: distortion.x = distortion.y = Random.Range(5, 10);
-//		case 9: break;
-//		case 8:	 distortion.x = distortion.y = Random.Range(3, 5);
-//		case 7: break;
-//		case 6:  distortion.x = distortion.y = Random.Range(1, 3);
-//		case 5: break;
-//		case 4: distortion.x = distortion.y = Random.Range(0, 1);
-//		case 3: break;
-//		case 2: distortion.x = distortion.y = Random.Range(0, 0);
-//		case 1: break;
-//		default: break;
-//		}
-		transform.position = Initial + distortion;
+		switch(GameObject.Find("Master").GetComponent<Master>().FailureScale)
+		{
+		case 10: 
+		case 9: distortion.x = Random.Range(-10, 10); distortion.y = Random.Range(-10, 10);break;
+		case 8:	 
+		case 7: distortion.x = Random.Range(-5, 5); distortion.y = Random.Range(-5, 5);break;
+		case 6:  
+		case 5: distortion.x = Random.Range(-3, 3); distortion.y = Random.Range(-3, 3);break;
+		case 4: 
+		case 3: distortion.x = Random.Range(-1, 1); distortion.y = Random.Range(-1, 1);break;
+		case 2: 
+		case 1:
+		default: break;
+		}
+			transform.position = Initial + distortion * 5;
+			frame = 0;
+		}
 	}
 }
