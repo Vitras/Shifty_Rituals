db63bb159aaa4656571cb86b12cafeb3ff9b5a0a
Dwight Peters <petersdwight@hotmail.com>
2016-01-30 21:55:41 +0100

    backgrounds and art import

--- MobileGame/Assets/WinLose.cs
+++ MobileGame/Assets/WinLose.cs
@@ -1,23 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class WinLose : MonoBehaviour {
 
     public AudioClip[] clips;
+    public Sprite[] backgrounds;
     private AudioSource source;
     private GameManager master;
 
 	// Use this for initialization
 	void Start ()
     {
-        Debug.Log(GameObject.Find("MasterObject"));
         master = GameObject.Find("MasterObject").GetComponent<GameManager>();
         source = GetComponent<AudioSource>();
+        Image background = GameObject.Find("Image").GetComponent<Image>();
         if (master.succes)
+        {
             source.clip = clips[0];
+            background.sprite = backgrounds[0];
+        }
         else
+        {
             source.clip = clips[1];
+            background.sprite = backgrounds[1];
+        }
         source.Play();
         StartCoroutine(wait2Sec());
 
