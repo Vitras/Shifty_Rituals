8bd0da70ba0df81dc3d398fd17b427397414aa1f
Dwight Peters <petersdwight@hotmail.com>
2016-01-31 11:06:02 +0100

    score + disconnect part 1

---
+++ ScreenGame/Assets/Score.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        GetComponent<Text>().text = GameObject.Find("Master").GetComponent<Master>().AchievedRounds.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
