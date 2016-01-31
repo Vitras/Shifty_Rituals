dbaff7cfb733cfebc478f5e0f3f5f1e9a1d2e5b2
Chrit Hameleers <chrithameleers@hotmail.com>
2016-01-31 12:35:35 +0100

    SMOKE INTENSIFIES

---
+++ ScreenGame/Assets/Scripts/Hoverscript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hoverscript : MonoBehaviour {
+
+	public float offset = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (transform.position.x, transform.position.y + Mathf.Sin (Time.time + offset) / 25, transform.position.y);
+	}
+}
