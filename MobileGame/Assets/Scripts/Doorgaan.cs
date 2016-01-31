3590eceb7d6f706291573ed2183066e85b2197e9
Dwight Peters <petersdwight@hotmail.com>
2016-01-31 02:08:46 +0100

    Difficulty added and pour merged

---
+++ MobileGame/Assets/Scripts/Doorgaan.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Doorgaan : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.touchCount == 0)
+            return;
+        if (Input.GetTouch(0).phase == TouchPhase.Ended)
+            DontDestroyOnLoad(GameObject.Find("MasterObject"));
+            Application.LoadLevel(Application.loadedLevel + 1);
+	}
+}
